af4faad238414678369b15fa4caac005bd4b6978
MayNiklas <info@niklas-steffen.de>
2021-03-07 18:13:26 +0100

    ↗️ Importing modules from MayNiklas

--- home.nix
+++ home.nix
@@ -1,6 +1,12 @@
 { config, pkgs, ... }:
 
-{
+let
+  mayniklas = builtins.fetchGit {
+    # Updated 2020-03-07
+    url = "https://github.com/mayniklas/nixos-home";
+    rev = "cb1517d09b0995d4c7bad8424e7baa56b68c31c4";
+  };
+in {
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
@@ -9,6 +15,12 @@
   home.username = "chris";
   home.homeDirectory = "/home/chris";
 
+  # Imports
+  imports = [
+    "${mayniklas}/modules/chromium.nix"
+    "${mayniklas}/modules/devolopment.nix"
+    "${mayniklas}/modules/firefox.nix"
+  ];
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
